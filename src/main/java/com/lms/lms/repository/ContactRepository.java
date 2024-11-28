703eedd1eacbe26b9d6da54fdabb1dfdb194b88a
Shiwani-R <117500933+Shiwani-R@users.noreply.github.com>
2024-11-28 20:58:15 +0530

    Delete src directory

--- src/main/java/com/lms/lms/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lms.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lms.lms.model.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
